00d48cb39b10e804e3834f2e84224c8f31ac8063
oOBobbyOo <944831575@qq.com>
2024-05-28 14:26:54 +0800

    refactor: change toogleDark

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <a class="select-none" title="Toggle Dark Mode" @click="toggleDark()">
+  <a class="select-none" title="Toggle Dark Mode" @click="toggleDark">
     <div i-ri-sun-line dark:i-ri-moon-line />
   </a>
 </template>
