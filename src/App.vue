1f8757c0e686d74410bed5f675288f12362a8928
oOBobbyOo <944831575@qq.com>
2024-06-04 17:11:38 +0800

    refactor: change App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
   <NavBar />
   <main class="of-x-hidden px-7 py-10">
     <RouterView />
+    <Footer :key="route.path" />
   </main>
-  <Footer />
 </template>
