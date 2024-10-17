b27337c34e53466c4e48195c01e957c4df146067
oOBobbyOo <944831575@qq.com>
2024-10-17 10:42:24 +0800

    chore: change MarkdownItMagicLink

---
+++ scripts/links.ts
@@ -0,0 +1,55 @@
+export const linksMap = {
+  'React': 'https://github.com/facebook/react',
+  'React Router': 'https://github.com/remix-run/react-router',
+  'Next.js': 'https://github.com/vercel/next.js',
+  'Zustand': 'https://github.com/pmndrs/zustand',
+  'Recoil': 'https://github.com/facebookexperimental/Recoil',
+  'Redux Toolkit': 'https://github.com/reduxjs/redux-toolkit',
+
+  'Vue': 'https://github.com/vuejs/core',
+  'Vue Router': 'https://github.com/vuejs/router',
+  'Nuxt': 'https://github.com/nuxt/nuxt',
+  'Pinia': 'https://github.com/vuejs/pinia',
+  'VueUse': 'https://github.com/vueuse/vueuse',
+  'Vitest': 'https://github.com/vitest-dev/vitest',
+
+  'Vite': 'https://github.com/vitejs/vite',
+  'Webpack': 'https://github.com/webpack/webpack',
+  'Esbuild': 'https://github.com/evanw/esbuild',
+  'Rollup': 'https://github.com/rollup/rollup',
+
+  'Javascript': 'https://github.com/tc39',
+  'TypeScript': 'https://github.com/microsoft/TypeScript',
+
+  'UnoCSS': 'https://github.com/unocss/unocss',
+  'TailwindCSS': 'https://github.com/tailwindlabs/tailwindcss',
+  'Styled Components': 'https://github.com/styled-components/styled-components',
+
+  'Sass': 'https://github.com/sass/sass',
+  'Less': 'https://github.com/less/less.js',
+  'Stylus': 'https://github.com/stylus/stylus',
+  'CSS Modules': 'https://github.com/css-modules/css-modules',
+
+  'Pnpm': 'https://github.com/pnpm/pnpm',
+  'Git': 'https://github.com/git/git',
+  'Eslint': 'https://github.com/eslint/eslint',
+  'Prettier': 'https://github.com/prettier/prettier',
+  'Husky': 'https://github.com/typicode/husky',
+  'Lint-staged': 'https://github.com/okonet/lint-staged',
+  'Commitlint': 'https://github.com/conventional-changelog/commitlint',
+
+  'Echarts': 'https://github.com/apache/echarts',
+  'Antv': 'https://github.com/antvis',
+  'ant-design-charts': 'https://github.com/ant-design/ant-design-charts',
+  'VisActor': 'https://github.com/visactor',
+}
+
+export const imageOverrides = [
+  ['https://github.com/facebook/react', 'https://react.dev/favicon-16x16.png'],
+  ['https://github.com/remix-run/react-router', 'https://reactrouter.com/favicon-light.png'],
+  ['https://github.com/facebookexperimental/Recoil', 'https://recoiljs.org/img/favicon.png'],
+  ['https://github.com/vuejs/core', 'https://vuejs.org/logo.svg'],
+  ['https://github.com/nuxt/nuxt', 'https://nuxt.com/assets/design-kit/icon-green.svg'],
+  ['https://github.com/vuejs/pinia', 'https://pinia.vuejs.org/logo.svg'],
+  ['https://github.com/vitejs/vite', 'https://vitejs.dev/logo.svg'],
+]
